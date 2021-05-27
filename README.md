a21c5a801e5a1f1a11a696a0735c4717e3c59c22
Pushkar <pushkar.jadhao@gmail.com>
2021-05-27 17:08:24 +0530

    redme updated and minor tweaks

---
+++ README.md
@@ -0,0 +1,26 @@
+# GetCount
+###for Count API
+
+GetCount is a tool to manage the links generated with Count API and will help you track the real time website visits through those links.
+
+Know more about Count API [here!](https://countapi.xyz/)
+
+
+
+##Generate your links by using below url
+`https://api.countapi.xyz/create?namespace={website_url}&key={pagename}&value=0`
+
+##Add below url on your website
+`https://api.countapi.xyz/update/{website_name}/{pagename}/?amount=1`
+
+call the above link from your webpage, so whenever the webpage is requested, the counter will increament by 1
+
+##Manage your links and track live count by GetCount
+
+`Run the script`
+
+`Input New Link` -> `https://api.countapi.xyz/get/{website_url}/{pagename}`
+
+Data is stored in `total_db.json` in the project dir
+
+To view live count, run the script again and select `View Live Count`
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 import os
 import urllib
-from collections import defaultdict
 from urllib import request
 
 count = 0
@@ -12,10 +11,17 @@ total_dic = {}
 
 
 def input_data():
-    # get_name = input("Enter a Name: ")
-    link_name.append(input("Enter a Name: "))
-    # get_link = input("Enter the Link: ")
-    all_links.append(input("Enter the Link: "))
+    print("=========================")
+    get_name = input("Enter a Name: ")
+    get_link = input("Enter the Link: ")
+    if get_name == "" or get_link == "":
+        print("*************************")
+        print("Enter A Valid Data")
+        input_data()
+    else:
+        link_name.append(get_name)
+        all_links.append(get_link)
+    print("=========================")
 
     def input_repeat():
         # open the existing file and update total_dir
@@ -37,13 +43,16 @@ def input_data():
             json.dump(total_dic, outfile)
 
     input_repeat()
-    print("*********************************************")
+    print("*************************")
     print("New Entry Added")
 
     def choice_two():
+        print("=========================")
         print("1. Add More Links")
         print("2. View Live Count")
         print("3. Exit")
+        print("=========================")
+
 
         ch = int(input("Enter Your Choice: "))
 
@@ -61,6 +70,7 @@ def input_data():
 
 
 def view_data():
+    total_dic.clear()
     global count
     with open("total_db.json", encoding="utf8") as infile:
         # check if the file is empty, if yes, then pass
@@ -69,6 +79,9 @@ def view_data():
         else:
             pass
 
+        print("=========================")
+        print("Live Count Are As Follows: ")
+        print("=========================")
         for key in dict(total_dic).keys():
             link_name.append(key)
         for value in dict(total_dic).values():
@@ -77,13 +90,31 @@ def view_data():
             file = urllib.request.urlopen(items)
             for line in file:
                 decoded_line = line.decode("utf-8")
-                print(link_name[count] + decoded_line[8:-1])
+                print(link_name[count] + ":\t" + decoded_line[9:-1])
+                print("*************************")
                 count += 1
 
 
 def choice():
+    print(r'''
+
+    ██████╗ ███████╗████████╗ ██████╗ ██████╗ ██╗   ██╗███╗   ██╗████████╗
+    ██╔════╝ ██╔════╝╚══██╔══╝██╔════╝██╔═══██╗██║   ██║████╗  ██║╚══██╔══╝
+    ██║  ███╗█████╗     ██║   ██║     ██║   ██║██║   ██║██╔██╗ ██║   ██║   
+    ██║   ██║██╔══╝     ██║   ██║     ██║   ██║██║   ██║██║╚██╗██║   ██║   
+    ╚██████╔╝███████╗   ██║   ╚██████╗╚██████╔╝╚██████╔╝██║ ╚████║   ██║   
+     ╚═════╝ ╚══════╝   ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   
+
+                a tool to manage Count API links
+                            by
+                        fortyseven                                                                      
+                https://fortysev-en.github.io''')
+    print("                         ")
+    print("=========================")
     print("1. Input New Link")
     print("2. View Live Count")
+    print("3. Exit")
+    print("=========================")
 
     ch = int(input("Enter Your Choice: "))
 
@@ -92,6 +123,9 @@ def choice():
 
     elif ch == 2:
         view_data()
+
+    elif ch == 3:
+        pass
 
 
 choice()

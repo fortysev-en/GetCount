d3997746a2c72c1e65106703ec3e8aa4129ce567
Pushkar <pushkar.jadhao@gmail.com>
2021-05-27 23:28:51 +0530

    link dict bug fixed

--- main.py
+++ main.py
@@ -14,35 +14,38 @@ def input_data():
     print("=========================")
     get_name = input("Enter a Name: ")
     get_link = input("Enter the Link: ")
-    if get_name == "" or get_link == "":
-        print("*************************")
-        print("Enter A Valid Data")
-        input_data()
-    else:
-        link_name.append(get_name)
-        all_links.append(get_link)
+    # if get_name == "" or get_link == "":
+    #     print("*************************")
+    #     print("Enter A Valid Data")
+    #     input_data()
+    # else:
+    link_name.append(get_name)
+    all_links.append(get_link)
     print("=========================")
 
-    def input_repeat():
+    def get_data():
+        if not os.path.exists('total_db.json'):
+            open("total_db.json", "w")
         # open the existing file and update total_dir
         with open("total_db.json", encoding="utf8") as infile:
             #check if the file is empty, if yes, then pass
             if os.stat("total_db.json").st_size != 0:
                 total_dic.update(json.load(infile))
+                print(total_dic)
             else:
                 pass
 
+    def update_data():
         # add user input key value to total_dic
-        for key in link_name:
-            for value in all_links:
-                total_dic[key] = value
-                break
+        res = {link_name[i]: all_links[i] for i in range(len(link_name))}
+        total_dic.update(res)
 
         # wtite the file with updated data
         with open("total_db.json", "w") as outfile:
             json.dump(total_dic, outfile)
 
-    input_repeat()
+    get_data()
+    update_data()
     print("*************************")
     print("New Entry Added")
 
@@ -53,12 +56,10 @@ def input_data():
         print("3. Exit")
         print("=========================")
 
-
         ch = int(input("Enter Your Choice: "))
 
         if ch == 1:
             input_data()
-            input_repeat()
 
         elif ch == 2:
             view_data()
